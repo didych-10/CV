0a9df733b1d4f7b61019a7445fdd358fbe48cd05
Didych Oleksandr <lilsanya044@icloud.com>
2020-11-12 18:54:37 +0200

    add all component

--- my-cv-app/src/app/components/skills/skills.component.ts
+++ my-cv-app/src/app/components/skills/skills.component.ts
@@ -7,6 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  hardSkills: any[] = [
+    {
+      name: 'HTML',
+      url: '/assets/img/html.jpg'
+    },
+
+    {
+      name: 'CSS',
+      url: '/assets/img/css.jpg'
+
+    },
+    {
+      name: 'Java script/type script',
+      url: '/assets/img/jsts.jpg'
+    },
+    {
+      name: 'Bootstrap',
+      url: '/assets/img/bootstrap.png'
+
+    },
+    {
+      name: 'Node js (basic)',
+      url: '/assets/img/nodejs.jpg'
+
+    },
+    {
+      name: 'MeSQL (basic)',
+      url: '/assets/img/sql.jpg'
+
+    },
+    {
+      name: 'Git/Github',
+      url: '/assets/img/git.png'
+
+    },
+    {
+      name: 'Angular',
+      url: '/assets/img/angular.png'
+
+    }
+
+  ];
+
+  softSkills: any[] = [
+    {
+      name: 'English (Intermediate)',
+      url: '/assets/img/eng.png'
+    },
+
+    {
+      name: 'Time management',
+      url: '/assets/img/time.jpg'
+    },
+
+    {
+      name: 'Сommunication',
+      url: '/assets/img/hand.jpg'
+
+    },
+    {
+      name: 'Excellent team player',
+      url: '/assets/img/team.jpg'
+    },
+    {
+      name: 'Detail oriented',
+      url: '/assets/img/detail.jpg'
+    }
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
